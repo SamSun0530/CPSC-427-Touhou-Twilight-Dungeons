0ed074cff37c260ba33e5b74e183c3dce12e2fa3
jiajun03 <jiajun03@student.ubc.ca>
2024-03-01 00:27:40 -0800

    Add decision tree setup

--- A1_template/src/ai_system.hpp
+++ A1_template/src/ai_system.hpp
@@ -5,13 +5,21 @@
 
 #include "tiny_ecs_registry.hpp"
 #include "common.hpp"
+#include "decision_tree.hpp"
 
 class AISystem
 {
 public:
 	AISystem();
 
+	// Initialize decision trees
+	void init();
+
 	void step(float elapsed_ms);
+
+private:
+	// Decision trees for different ai entities
+	DecisionTree ghost;
 
 	// C++ random number generator
 	std::default_random_engine rng;
---
+++ A1_template/src/ai_system_init.cpp
@@ -0,0 +1,5 @@
+#include "ai_system.hpp"
+
+void AISystem::init() {
+
+}
