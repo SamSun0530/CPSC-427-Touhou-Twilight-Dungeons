f244134c7ee9261970e12655858baa16b077197f
jiajun03 <jiajun03@students.cs.ubc.ca>
2024-04-01 22:26:14 -0700

    Fix texture seam artifacts by adding border padding

--- A1_template/shaders/tiles_instance.fs.glsl
+++ A1_template/shaders/tiles_instance.fs.glsl
@@ -13,6 +13,12 @@ layout(location = 0) out vec4 color;
 
 void main()
 {
+    // Each texture in the atlas has 1 pixel border padding (clamped colors instead of transparent) to prevent seam artifacts
+    // Or texture bleeding from adjacent textures
+    // Another way to fix this is by setting GL_NEAREST instead of GL_LINEAR to prevent pixel interpolation
+    // Size of one pixel
+    // TODO: Create generic texture atlas class
+    vec2 pixel = vec2(1.0) / vec2(306.0,238.0); 
     // Adapted from: https://gamedev.stackexchange.com/a/86356
-    color = vec4(fcolor, 1.0) * texture(sampler0, texcoord * sprite_loc.zw + sprite_loc.xy);
+    color = vec4(fcolor, 1.0) * texture(sampler0, texcoord * (sprite_loc.zw - pixel * 2.0) + sprite_loc.xy + pixel);
 }
